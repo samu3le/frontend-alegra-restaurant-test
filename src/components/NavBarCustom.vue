314721c8e3b2a80fa7dd7c4416fce56cf55335d4
Samuel Espinoza <samuelesponline@gmail.com>
2022-02-18 11:51:31 -0500

    Navbar add nickname - [SECTION] navbar - [TYPE] fix

--- src/components/NavBarCustom.vue
+++ src/components/NavBarCustom.vue
@@ -1,43 +1,73 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-      <button
-        type="button"
-        id="sidebarCollapse"
-        @click="isOpenSideBarChange"
-        class="btn btn-dark d-inline-block ml-auto"
-      >
-        <svg
-          v-if="!isOpenSideBar"
-          style="width: 0.875em"
-          aria-hidden="true"
-          data-prefix="fas"
-          data-icon="align-left"
-          role="img"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 448 512"
-          data-fa-i2svg=""
+      <div class="col-md-10">
+        <button
+          type="button"
+          id="sidebarCollapse"
+          @click="isOpenSideBarChange"
+          class="btn btn-dark d-inline-block ml-auto"
         >
-          <path
-            fill="currentColor"
-            d="M288 44v40c0 8.837-7.163 16-16 16H16c-8.837 0-16-7.163-16-16V44c0-8.837 7.163-16 16-16h256c8.837 0 16 7.163 16 16zM0 172v40c0 8.837 7.163 16 16 16h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16zm16 312h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm256-200H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16h256c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16z"
-          ></path>
-        </svg>
-        <svg
-          v-else
-          style="width: 0.875em"
-          version="1.1"
-          viewBox="0 0 512 512"
-          xml:space="preserve"
-          xmlns="http://www.w3.org/2000/svg"
-          xmlns:xlink="http://www.w3.org/1999/xlink"
-        >
-          <path
-            fill="currentColor"
-            d="M443.6,387.1L312.4,255.4l131.5-130c5.4-5.4,5.4-14.2,0-19.6l-37.4-37.6c-2.6-2.6-6.1-4-9.8-4c-3.7,0-7.2,1.5-9.8,4  L256,197.8L124.9,68.3c-2.6-2.6-6.1-4-9.8-4c-3.7,0-7.2,1.5-9.8,4L68,105.9c-5.4,5.4-5.4,14.2,0,19.6l131.5,130L68.4,387.1  c-2.6,2.6-4.1,6.1-4.1,9.8c0,3.7,1.4,7.2,4.1,9.8l37.4,37.6c2.7,2.7,6.2,4.1,9.8,4.1c3.5,0,7.1-1.3,9.8-4.1L256,313.1l130.7,131.1  c2.7,2.7,6.2,4.1,9.8,4.1c3.5,0,7.1-1.3,9.8-4.1l37.4-37.6c2.6-2.6,4.1-6.1,4.1-9.8C447.7,393.2,446.2,389.7,443.6,387.1z"
-          />
-        </svg>
-      </button>
+          <svg
+            v-if="!isOpenSideBar"
+            style="width: 0.875em"
+            aria-hidden="true"
+            data-prefix="fas"
+            data-icon="align-left"
+            role="img"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 448 512"
+            data-fa-i2svg=""
+          >
+            <path
+              fill="currentColor"
+              d="M288 44v40c0 8.837-7.163 16-16 16H16c-8.837 0-16-7.163-16-16V44c0-8.837 7.163-16 16-16h256c8.837 0 16 7.163 16 16zM0 172v40c0 8.837 7.163 16 16 16h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16zm16 312h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm256-200H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16h256c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16z"
+            ></path>
+          </svg>
+          <svg
+            v-else
+            style="width: 0.875em"
+            version="1.1"
+            viewBox="0 0 512 512"
+            xml:space="preserve"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+          >
+            <path
+              fill="currentColor"
+              d="M443.6,387.1L312.4,255.4l131.5-130c5.4-5.4,5.4-14.2,0-19.6l-37.4-37.6c-2.6-2.6-6.1-4-9.8-4c-3.7,0-7.2,1.5-9.8,4  L256,197.8L124.9,68.3c-2.6-2.6-6.1-4-9.8-4c-3.7,0-7.2,1.5-9.8,4L68,105.9c-5.4,5.4-5.4,14.2,0,19.6l131.5,130L68.4,387.1  c-2.6,2.6-4.1,6.1-4.1,9.8c0,3.7,1.4,7.2,4.1,9.8l37.4,37.6c2.7,2.7,6.2,4.1,9.8,4.1c3.5,0,7.1-1.3,9.8-4.1L256,313.1l130.7,131.1  c2.7,2.7,6.2,4.1,9.8,4.1c3.5,0,7.1-1.3,9.8-4.1l37.4-37.6c2.6-2.6,4.1-6.1,4.1-9.8C447.7,393.2,446.2,389.7,443.6,387.1z"
+            />
+          </svg>
+        </button>
+      </div>
+
+      <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav">
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdown"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                <!--! Font Awesome Pro 6.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+                <path
+                  d="M224 256c70.7 0 128-57.31 128-128s-57.3-128-128-128C153.3 0 96 57.31 96 128S153.3 256 224 256zM274.7 304H173.3C77.61 304 0 381.6 0 477.3c0 19.14 15.52 34.67 34.66 34.67h378.7C432.5 512 448 496.5 448 477.3C448 381.6 370.4 304 274.7 304z"
+                />
+              </svg>
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><hr class="dropdown-divider" /></li>
+              <li><a class="dropdown-item" href="#">Sign Out</a></li>
+            </ul>
+          </li>
+        </ul>
+        <span class="navbar-text"> Nickname </span>
+      </div>
     </div>
   </nav>
 </template>
@@ -66,6 +96,14 @@ export default {
 </script>
 
 <style scoped>
+.dropdown {
+  width: 3em;
+}
+
+.li {
+  align-content: center;
+}
+
 .navbar {
   padding: 15px 10px;
   background: #fff;
--- src/endpoint/index.js
+++ src/endpoint/index.js
@@ -1,78 +1,80 @@
 import axios from "axios";
 
 const baseURL =
-    process.env.VUE_APP_ENDPOINT_URL ??
-    "http://test-alegra-restaurant.test/api/v1/";
+  process.env.VUE_APP_ENDPOINT_URL ??
+  "http://test-alegra-restaurant.test/api/v1/";
 
 const format = "json";
 const tz_in_minutes = new Date().getTimezoneOffset();
 
 const headers = {
-    "Content-Type": "application/json",
-    Accept: "application/json",
-    tz_in_minutes,
-    lg: navigator.language || navigator.userLanguage,
-    device: navigator.userAgent,
+  "Content-Type": "application/json",
+  Accept: "application/json",
+  tz_in_minutes,
+  lg: navigator.language || navigator.userLanguage,
+  device: navigator.userAgent,
+  Authorization:
+    "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1dWlkIjoiZDc1ZjI4NmMtOWZkZS00YzliLWE4ZTYtNDQ2NjY4NWQxOTFkIiwiZXhwIjoxNjQ1NzYyOTExfQ.1EkAAMv9-bLBCT6mFDWCUOjlFvxXlul7GkxzKGIZPYQ",
 };
 
 const instance = axios.create({
-    mode: "cors",
-    cache: "no-cache",
-    baseURL,
-    headers,
-    redirect: "follow",
-    referrerPolicy: "no-referrer",
+  mode: "cors",
+  cache: "no-cache",
+  baseURL,
+  headers,
+  redirect: "follow",
+  referrerPolicy: "no-referrer",
 });
 
 instance.interceptors.response.use(
-    (response) => response,
-    (error) => {
-        const { response } = error;
-        const { data, status } = response;
+  (response) => response,
+  (error) => {
+    const { response } = error;
+    const { data, status } = response;
 
-        const res = {};
+    const res = {};
 
-        switch (status) {
-            case 422:
-                const { errors, message } = data;
-                res["errors"] = errors ? errors : data;
-                break;
+    switch (status) {
+      case 422:
+        const { errors, message } = data;
+        res["errors"] = errors ? errors : data;
+        break;
 
-            case 419:
-                window.location.reload();
-                break;
+      case 419:
+        window.location.reload();
+        break;
 
-            default:
-                res["errors"] = ["Error no controlado"];
-                break;
-        }
-
-        return Promise.reject(res);
+      default:
+        res["errors"] = ["Error no controlado"];
+        break;
     }
+
+    return Promise.reject(res);
+  }
 );
 
 export default {
-    get({ url, params }) {
-        return instance.get(url, {
-            params: {
-                ...params,
-                format,
-                tz_in_minutes,
-            },
-        });
-    },
+  get({ url, params }) {
+    return instance.get(url, {
+      params: {
+        ...params,
+        format,
+        tz_in_minutes,
+      },
+    });
+  },
 
-    post({ url, params }) {
-        console.log("endpoint -> params", params);
-        const param_with_format = {
-            ...params,
-            format,
-            tz_in_minutes,
-        };
-        return instance.post(url, param_with_format, {
-            params: {
-                tz_in_minutes,
-            },
-        });
-    },
+  post({ url, params }) {
+    console.log("endpoint -> params", params);
+    const param_with_format = {
+      ...params,
+      format,
+      tz_in_minutes,
+    };
+    return instance.post(url, param_with_format, {
+      params: {
+        tz_in_minutes,
+      },
+    });
+  },
 };
