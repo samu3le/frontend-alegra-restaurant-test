48e9dd509e4d45d9c68397bd401be9b95e88764e
Samuel Espinoza <samuelesponline@gmail.com>
2022-02-19 17:51:05 -0500

    - [SECTION] - [TYPE]

--- src/store/config/getters.js
+++ src/store/config/getters.js
@@ -1,19 +1,22 @@
 import store from "@/store";
 
 export default {
-    getMenuItems( state, ) {
-        const auth = store.getters['auth/auth'];
-        let items = state.menu.items
-            .filter(({ requiresAuth }) => requiresAuth == !!auth);
-        if ( !!auth ) {
-            items = items.filter(item => item.roles.includes(auth.role));
-        }
-        return items;
-    },
-    isOpenSideBar( state, ) {
-        return state.isOpenSideBar
-    },
-    isOpenNavBar( state, ) {
-        return state.isOpenNavBar
-    },
-}
+  getMenuItems(state) {
+    const auth = store.getters["auth/auth"];
+    let items = state.menu.items.filter(
+      ({ requiresAuth }) => requiresAuth == !!auth
+    );
+    if (!!auth) {
+      items = items.filter((item) =>
+        item.roles ? item.roles.includes(auth.role) : true
+      );
+    }
+    return items;
+  },
+  isOpenSideBar(state) {
+    return state.isOpenSideBar;
+  },
+  isOpenNavBar(state) {
+    return state.isOpenNavBar;
+  },
+};
